4af8c2b667239b7d7c76f32be9b54737d4771c02
AnguleMathias <angulemathias3@gmail.com>
2019-01-25 07:44:08 +0300

    Type relations

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -11,9 +11,9 @@ const {
 
 // dummy data books
 var books = [
-    {name:'Name of the Wind', genre:'Fantasy', id:'1'},
-    {name:'The Final Empire', genre:'Fantasy', id:'2'},
-    {name:'The Long earth', genre:'Sci-Fi', id:'3'},
+    {name:'Name of the Wind', genre:'Fantasy', id:'1', authorId:'1'},
+    {name:'The Final Empire', genre:'Fantasy', id:'2', authorId:'2'},
+    {name:'The Long Earth', genre:'Sci-Fi', id:'3', authorId:'3'},
 ];
 
 // dummy data author
@@ -29,7 +29,13 @@ const BookType = new GraphQLObjectType({
     fields: () => ({
         id: {type: GraphQLID},
         name: {type: GraphQLString},
-        genre: {type: GraphQLString}
+        genre: {type: GraphQLString},
+        author: {
+            type: AuthorType,
+            resolve(parent, args){
+                return _.find(authors, {id:parent.authorId});
+            }
+        }
     })
 });
 
