2ccaf084e9017c98c3dc0db8d00514170e502473
AnguleMathias <angulemathias3@gmail.com>
2019-01-25 08:51:00 +0300

    mLab connection

--- server/app.js
+++ server/app.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
 
 const app = express();
+
+// connect to mlab
+mongoose.connect('mongodb://mathias:test123@ds211865.mlab.com:11865/gql-bookstore', { useNewUrlParser: true })
+mongoose.connection.once('open', () => {
+    console.log('connected to database');
+});
 
 app.use('/graphql', graphqlHTTP({
     schema,
